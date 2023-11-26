b355d9fd1e0b581b0d6b0bc6e64b1160c8122f2d
PedrobyJoao <joaoexplorer235@gmail.com>
2023-11-26 11:49:18 -0300

    feat: if 'x' pressed, close both tabs as in vimium

--- js/confirm.js
+++ js/confirm.js
@@ -11,3 +11,12 @@ document.getElementById('cancelBtn').addEventListener('click', function() {
     // Close the current confirmation tab.
     window.close();
 });
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'x' || event.key === 'X') {
+        // Close the Reddit or YouTube tab.
+        chrome.tabs.remove(targetTabId);
+        // Close the current confirmation tab.
+        window.close();
+    }
+});
