93069a7c7b51d3b0d72da0c1d3c57250972c9cea
PedrobyJoao <joaoexplorer235@gmail.com>
2023-11-26 11:49:18 -0300

    refact: refactoring on the code organization

--- js/confirm.js
+++ js/confirm.js
@@ -1,15 +1,13 @@
-const redditTabId = parseInt(location.hash.substring(1));
-
+const targetTabId = parseInt(location.hash.substring(1));
 
 document.getElementById('confirmBtn').addEventListener('click', function() {
-    // Continue to Reddit.
-    window.close();
-});
-
-document.getElementById('cancelBtn').addEventListener('click', function() {
-    // Close the Reddit tab.
-    chrome.tabs.remove(redditTabId);
     // Close the current confirmation tab.
     window.close();
 });
 
+document.getElementById('cancelBtn').addEventListener('click', function() {
+    // Close the Reddit or YouTube tab.
+    chrome.tabs.remove(targetTabId);
+    // Close the current confirmation tab.
+    window.close();
+});
